6e04bcfe9b160d1930c769d1abf10770aeffd6ec
Victor Moreira Acacio <victor.mor.acacio@gmail.com>
2026-04-18 20:29:06 -0300

    Definição dos parâmetros de entrada

---
+++ src/parameters.py
@@ -0,0 +1,6 @@
+# -------------------------------------------------- Parâmetros de Entrada --------------------------------------------------
+
+z = 1  # Redshift [adm.]
+H0 = 68  # Constante de Hubble [km/s/Mpc]
+Omega_M = 0.3  # Densidade de matéria [adm.]
+Omega_EE = 0.7  # Densidade de energia escura [adm.]
