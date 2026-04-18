ce7b25f3c448a5788cea89fd1ad2d2ba5978b142
Victor Moreira Acacio <victor.mor.acacio@gmail.com>
2026-04-18 20:54:32 -0300

    Atualização dos parametros, adição da maioria das funções do core.

--- src/parameters.py
+++ src/parameters.py
@@ -1,6 +1,11 @@
+#-------------------------------------------------- Bibliotecas --------------------------------------------------
+
+from astropy import constants as const
+
 # -------------------------------------------------- Parâmetros de Entrada --------------------------------------------------
 
-z = 1  # Redshift [adm.]
 H0 = 68  # Constante de Hubble [km/s/Mpc]
 Omega_M = 0.3  # Densidade de matéria [adm.]
 Omega_EE = 0.7  # Densidade de energia escura [adm.]
+c=const.c.to('km/s').value  # Velocidade da luz [km/s]
+w=-1 # Parâmetro de equação de estado da energia escura [adm.]
