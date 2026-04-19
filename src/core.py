e5297e985b1f4aae361a1e103ee64bb77cc90221
Victor Moreira Acacio <victor.mor.acacio@gmail.com>
2026-04-18 23:43:49 -0300

    Finalizado a formalização para iniciar a integração

--- src/core.py
+++ src/core.py
@@ -1,52 +1,66 @@
-#-------------------------------------------------- Bibliotecas --------------------------------------------------
+# -------------------------------------------------- Bibliotecas --------------------------------------------------
 
 import numpy as np
 
 from src.parameters import *
 
-#-------------------------------------------------- Funções Fundamentais --------------------------------------------------
+# -------------------------------------------------- Funções Fundamentais --------------------------------------------------
+
 
 def q0(Omega_M, Omega_EE):
     """Calcula o parâmetro de desaceleração atual (q0) a partir dos parâmetros de densidade de matéria e energia escura."""
     return 0.5 * Omega_M - Omega_EE
 
+
 def dL(dm, z):
     """Calcula a distância de luminosidade (dL) a partir da distância comóvel transversal (dm) e do redshift (z)."""
     return (1 + z) * dm
 
+
 def approx_dL(Omega_M, Omega_EE, z):
     """Calcula a aproximação de distância de luminosidade (dL) para pequenos redshifts (z) usando o parâmetro de desaceleração (q0)."""
-    return (c/H0)*z*(1 + (1 - q0(Omega_M, Omega_EE))*z/2)
+    return (c / H0) * z * (1 + (1 - q0(Omega_M, Omega_EE)) * z / 2)
+
 
 def Omega_K(Omega_M, Omega_EE):
     """Calcula o parâmetro de curvatura (Omega_K) a partir dos parâmetros de densidade de matéria e energia escura."""
     return 1 - (Omega_M + Omega_EE)
 
+
 def mu(dm, z):
     """Calcula a magnitude de distância (mu) a partir da distância de luminosidade (dL) e do redshift (z)."""
-    return 5 * np.log10(dL(dm,z)) + 25
+    return 5 * np.log10(dL(dm, z)) + 25
+
 
 def E(z, Omega_M, Omega_EE):
     """Calcula a função de expansão (E) a partir do redshift (z) e dos parâmetros de densidade de matéria e energia escura."""
-    return np.sqrt(Omega_M * (1 + z)**3 + Omega_EE*(1+z)**3*(1+w)+ Omega_K(Omega_M, Omega_EE) * (1 + z)**2)
+    return np.sqrt(
+        Omega_M * (1 + z) ** 3
+        + Omega_EE * (1 + z) ** 3 * (1 + w)
+        + Omega_K(Omega_M, Omega_EE) * (1 + z) ** 2
+    )
 
-def Sk(k,r):
+
+def Sk(k, r):
     """Calcula a função de distância comóvel transversal (Sk) a partir do parâmetro de curvatura (k) e da distância comóvel radial (r)."""
     if k > 0:
-        return np.sin(np.sqrt(k)*r)/np.sqrt(k)
+        return np.sin(np.sqrt(k) * r) / np.sqrt(k)
     elif k < 0:
-        return np.sinh(np.sqrt(-k)*r)/np.sqrt(-k)
+        return np.sinh(np.sqrt(-k) * r) / np.sqrt(-k)
     else:
         return r
-    
-def dm(k,dC):
+
+
+def dm(k, dC):
     """Calcula a distância comóvel transversal (dm) a partir do parâmetro de curvatura (k) e da distância comóvel radial (dC)."""
-    return Sk(k,dC)
+    return Sk(k, dC)
+
 
 def int(z, Omega_M, Omega_EE):
     """Formaliza a quantidade a ser integrada para o cálculo da distância comóvel radial (dC) a partir do redshift (z) e dos parâmetros de densidade de matéria e energia escura."""
-    return 1/E(z, Omega_M, Omega_EE)
+    return 1 / E(z, Omega_M, Omega_EE)
+
 
 def dC(resint):
     """Calcula o valor de distância comóvel radial (dC) a partir do resultado da integração dda função int."""
-    return (c/H0)*resint
+    return (c / H0) * resint
--- src/parameters.py
+++ src/parameters.py
@@ -1,12 +1,15 @@
-#-------------------------------------------------- Bibliotecas --------------------------------------------------
+# -------------------------------------------------- Bibliotecas --------------------------------------------------
 
 from astropy import constants as const
 
-# -------------------------------------------------- Parâmetros de Entrada --------------------------------------------------
+# -------------------------------------------------- Constantes Físicas --------------------------------------------------
 
 H0 = 68  # Constante de Hubble [km/s/Mpc]
+c = const.c.to("km/s").value  # Velocidade da luz [km/s]
+
+# -------------------------------------------------- Parâmetros de Entrada --------------------------------------------------
+
 Omega_M = 0.3  # Densidade de matéria [adm.]
 Omega_EE = 0.7  # Densidade de energia escura [adm.]
-c=const.c.to('km/s').value  # Velocidade da luz [km/s]
-w=-1 # Parâmetro de equação de estado da energia escura [adm.]
-z=10 # Redshift máximo [adm.]
+w = -1  # Parâmetro de equação de estado da energia escura [adm.]
+z = 10  # Redshift máximo [adm.]
