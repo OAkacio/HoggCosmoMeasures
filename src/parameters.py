029c8cffb2bc05b44d82e601a2a2b42a0af59632
OAkacio <victor.mor.acacio@gmail.com>
2026-04-29 16:26:04 -0300

    Update parameters and figures to reflect new density values for matter and dark energy
    - Modified density values in `data/infos.txt` to 0.315 for matter and 0.685 for dark energy.
    - Updated various figures (`DIFdistribuicao.pdf`, `DLAPcomparacao.pdf`, `DLComparacao.pdf`, `DLdistribuicao.pdf`, `ExataAproximadadistribuicao.pdf`, `MUcomparacao.pdf`) to display the new density values.
    - Adjusted axis labels and values in figures for clarity and consistency with updated parameters.

--- src/parameters.py
+++ src/parameters.py
@@ -13,8 +13,8 @@ from src.constants import *
 # * PARÂMETROS INICIAIS
 # * =============================================================================
 
-Omega_M = 0.0  # Densidade de matéria [adm.]
-Omega_EE = 1.0  # Densidade de energia escura [adm.]
+Omega_M = 0.315  # Densidade de matéria [adm.]
+Omega_EE = 0.685  # Densidade de energia escura [adm.]
 w = -1  # Parâmetro de equação de estado da energia escura [adm.]
 z = 6  # Redshift máximo [adm.]
 
